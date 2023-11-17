b0767526d15be0c1a6ee0e46a230f41c797459c4
Sri Vishnu <srivishp@gmail.com>
2023-11-17 18:00:49 +0530

    Display all events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,5 +1,14 @@
+import { getAllEvents } from "@/dummy-data";
+import EventList from "@/components/events/EventList";
+
 function AllEventsPage() {
-  return <h1>The Events Page</h1>;
+  const events = getAllEvents();
+
+  return (
+    <div>
+      <EventList items={events} />
+    </div>
+  );
 }
 
 export default AllEventsPage;
