f7e6c1e7be5bbe9b4a51bbc027740e2fa9b695b2
Sri Vishnu <srivishp@gmail.com>
2024-01-19 12:33:35 +0530

    Add title and meta tags in the <Head> section

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
+import Head from "next/head";
 import EventList from "../components/events/EventList";
 import { getFeaturedEvents } from "../dummy-data";
 
 function HomePage() {
   const featuredEvents = getFeaturedEvents();
-  return <EventList items={featuredEvents} />;
+  return (
+    <>
+      {/* <Head> not <head> */}
+      <Head>
+        <title>NextJS Events</title>
+        {/* Data in the meta tags is important as search engines display it when results are found */}
+        <meta name="description" content="Attend many interesting events!" />
+      </Head>
+      <EventList items={featuredEvents} />;
+    </>
+  );
 }
 
 export default HomePage;
